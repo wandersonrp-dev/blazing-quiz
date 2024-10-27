585a861d4c2ed332d28832635e7b219d5108dc26
wandersonrp-dev <wandersonrp.dev@outlook.com>
2024-10-27 19:21:01 -0300

    feat: add authentication

--- src/Backend/BlazingQuiz.Api/Program.cs
+++ src/Backend/BlazingQuiz.Api/Program.cs
@@ -2,9 +2,13 @@ using BlazingQuiz.Api;
 using BlazingQuiz.Api.Data;
 using BlazingQuiz.Api.Endpoints;
 using BlazingQuiz.Api.Entities;
+using BlazingQuiz.Api.Extensions;
 using BlazingQuiz.Api.Filters;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +16,25 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration.GetJwtIssuer(),
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration.GetJwtAudience(),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetJwtSecret())),
+        ValidateIssuerSigningKey = true,
+    };
+});
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddDi();
 
@@ -35,6 +58,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.MapAuthEndpoints();
 
